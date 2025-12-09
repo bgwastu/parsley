69df3327816842eb63ac406d4cea0a83cb1536b4
Bagas Wastu <git@wastu.net>
2025-12-09 09:10:47 +0700

    chore: update icons

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,7 +2,7 @@ import { IconLeaf2 } from "@tabler/icons-react";
 
 export function AppLogo() {
 	return (
-		<div className="flex items-center justify-center w-10 h-10 bg-primary rounded-2xl">
+		<div className="flex items-center justify-center w-10 h-10 bg-primary rounded-md">
 			<IconLeaf2 className="w-6 h-6 text-primary-foreground" />
 		</div>
 	);
